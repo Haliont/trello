e31ae0791ab4c52a5828740745e127cbc9be6c8d
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-10-28 23:19:54 +0600

    updated .eslintrc.yml

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bulma/css/bulma.min.css";
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
