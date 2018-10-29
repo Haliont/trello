1672bb6a8c85043faba9f6301fb9165693023d55
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-10-29 19:00:01 +0600

    Refactored

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import List from './List';
 import CardModal from './CardModal';
 import { updateItem, uid } from '../helpers';
+
 import {
   make as makeLists,
   setListTitle,
@@ -10,6 +11,17 @@ import {
   removeCardFromList,
   getList,
 } from '../state/lists';
+
+import {
+  make as makeCards,
+  addCard,
+  removeCard,
+  setCardTitle,
+  setCardDesc,
+  getCard,
+  addCommentInCard,
+  removeCommentFromCard,
+} from '../state/cards';
 
 const initialLists = [
   {
@@ -39,7 +51,7 @@ class Board extends Component {
     super(props);
     this.state = {
       lists: makeLists(initialLists),
-      cards: [],
+      cards: makeCards([]),
       comments: [],
       modalData: {
         title: '',
@@ -72,32 +84,27 @@ class Board extends Component {
 
   addCardInList = listId => (newCardTitle) => {
     const newCardId = uid();
+
     const newCard = {
       id: newCardId,
       title: newCardTitle,
-      description: '',
+      desc: '',
       commentIds: [],
     };
 
-    this.setState(({ lists, cards }) => {
-      const newCards = [...cards, newCard];
-      return {
-        lists: addCardInList(listId, newCardId, lists),
-        cards: newCards,
-      };
-    });
+    this.setState(({ lists, cards }) => ({
+      lists: addCardInList(listId, newCardId, lists),
+      cards: addCard(newCard, cards),
+    }));
   };
 
   removeCardFromList = listId => cardId => (event) => {
     event.stopPropagation();
 
-    this.setState(({ lists, cards }) => {
-      const filteredCards = cards.filter(({ id }) => id !== cardId);
-      return {
-        lists: removeCardFromList(listId, cardId, lists),
-        cards: filteredCards,
-      };
-    });
+    this.setState(({ lists, cards }) => ({
+      lists: removeCardFromList(listId, cardId, lists),
+      cards: removeCard(cardId, cards),
+    }));
   };
 
   closeCardModal = () => {
@@ -105,55 +112,50 @@ class Board extends Component {
   };
 
   openCardModal = listId => cardId => () => {
-    const { cards, lists } = this.state;
+    this.setState(({ cards, lists }) => {
+      const { desc, title, commentIds } = getCard(cardId, cards);
+      const { title: listTitle } = getList(listId, lists);
 
-    const { title: cardTitle, description, commentIds } = cards.find(
-      ({ id }) => id === cardId,
-    );
-
-    const { title: listTitle } = getList(listId, lists);
-
-    const modalData = {
-      title: cardTitle,
-      commentIds,
-      description,
-      listTitle,
-      cardId,
-    };
-
-    this.setState({ isOpenModal: true, modalData });
+      return {
+        modalData: {
+          desc,
+          title,
+          cardId,
+          listTitle,
+          commentIds,
+        },
+        isOpenModal: true,
+      };
+    });
   };
 
-  updateCardTitle = cardId => (newTitle) => {
-    const { cards } = this.state;
-    const updatedCards = updateItem(cardId, { title: newTitle }, cards);
-    this.setState({ cards: updatedCards });
+  setCardTitle = cardId => (newTitle) => {
+    this.setState(({ cards }) => ({
+      cards: setCardTitle(cardId, newTitle, cards),
+    }));
   };
 
-  updateCardDescription = cardId => (newDescription) => {
-    const { cards } = this.state;
-    const updatedCards = updateItem(cardId, {
-      description: newDescription,
-    }, cards);
-    this.setState({ cards: updatedCards });
+  updateCardDesc = cardId => (newDesc) => {
+    this.setState(({ cards }) => ({
+      cards: setCardDesc(cardId, newDesc, cards),
+    }));
   };
 
   addCommentInCard = cardId => (commentText) => {
-    const { cards, comments } = this.state;
-    const { username } = this.props;
-    const newCommentId = uid();
-    const newComment = {
-      id: newCommentId,
-      text: commentText,
-      author: username,
-    };
-    const newComments = [...comments, newComment];
-    const { commentIds } = cards.find(({ id }) => cardId === id);
-    const newCommentIds = [...commentIds, newCommentId];
-    const updatedCards = updateItem(cardId, {
-      commentIds: newCommentIds,
-    }, cards);
-    this.setState({ comments: newComments, cards: updatedCards });
+    this.setState(({ cards, comments }, { username }) => {
+      const newCommentId = uid();
+
+      const newComment = {
+        id: newCommentId,
+        text: commentText,
+        author: username,
+      };
+
+      return {
+        comments: [...comments, newComment],
+        cards: addCommentInCard(cardId, newCommentId, cards),
+      };
+    });
   };
 
   updateComment = commentId => (newCommentText) => {
@@ -165,17 +167,10 @@ class Board extends Component {
   };
 
   removeCommentFromCard = cardId => commentId => () => {
-    const { cards, comments } = this.state;
-    const { commentIds } = cards.find(({ id }) => cardId === id);
-    const newCommentIds = commentIds.filter(id => commentId !== id);
-    const updatedCards = updateItem(cardId, {
-      commentIds: newCommentIds,
-    }, cards);
-    const newComments = comments.filter(({ id }) => commentId !== id);
-    this.setState({
-      comments: newComments,
-      cards: updatedCards,
-    });
+    this.setState(({ cards, comments }) => ({
+      comments: comments.filter(({ id }) => commentId !== id),
+      cards: removeCommentFromCard(cardId, commentId, cards),
+    }));
   };
 
   fetchData() {
@@ -216,7 +211,7 @@ class Board extends Component {
       cards,
       comments,
       modalData: {
-        title, description, listTitle, cardId,
+        title, desc, listTitle, cardId,
       },
     } = this.state;
 
@@ -226,11 +221,11 @@ class Board extends Component {
     return (
       <CardModal
         title={title}
-        description={description}
+        desc={desc}
         username={username}
         listTitle={listTitle}
-        updateTitle={this.updateCardTitle(cardId)}
-        updateDescription={this.updateCardDescription(cardId)}
+        updateTitle={this.setCardTitle(cardId)}
+        updateDesc={this.updateCardDesc(cardId)}
         comments={cardComments}
         onUpdateComment={this.updateComment}
         onRemoveComment={this.removeCommentFromCard(cardId)}
@@ -239,11 +234,11 @@ class Board extends Component {
       />
       // <CardModal
       //   title={title}
-      //   description={description}
+      //   desc={desc}
       //   username={username}
       //   listTitle={listTitle}
-      //   updateTitle={this.updateCardTitle(cardId)}
-      //   updateDescription={this.updateCardDescription(cardId)}
+      //   updateTitle={this.setCardTitle(cardId)}
+      //   updatedesc={this.updateCarddesc(cardId)}
       //   comments={cardComments}
       //   onUpdateComment={this.updateComment}
       //   onAddComment={this.addCommentInCard(cardId)}
