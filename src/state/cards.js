4d9aec39c839d9de88bc4aae5577093a612b546b
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-10-29 18:54:33 +0600

    Added abstraction for cards

---
+++ src/state/cards.js
@@ -0,0 +1,50 @@
+import { updateItem, uid } from '../helpers';
+
+/*
+cards = [
+  {
+    id: 0,
+    title: 'Title of card',
+    cardIds: [1, 2, 3],
+  }
+]
+* */
+
+const make = (cards = []) => cards;
+const getCard = (id, cards) => cards.find(card => id === card.id);
+
+const addCard = (
+  card = {
+    id: uid(),
+    title: 'Card title',
+    desc: '',
+    commentIds: [],
+  },
+  cards,
+) => make([...cards, card]);
+
+const setCardTitle = (cardId, title, cards) => updateItem(cardId, { title }, cards);
+const setCardDesc = (cardId, desc, cards) => updateItem(cardId, { desc }, cards);
+const removeCard = (cardId, cards) => cards.filter(card => cardId !== card.id);
+
+const addCommentInCard = (cardId, commentId, cards) => {
+  const { commentIds } = getCard(cardId, cards);
+  return updateItem(cardId, { commentIds: [...commentIds, commentId] }, cards);
+};
+
+const removeCommentFromCard = (cardId, commentId, cards) => {
+  const { commentIds } = getCard(cardId, cards);
+  const filteredCommentIds = commentIds.filter(id => id !== commentId);
+  return updateItem(cardId, { commentIds: filteredCommentIds }, cards);
+};
+
+export {
+  make,
+  getCard,
+  addCard,
+  setCardTitle,
+  setCardDesc,
+  removeCard,
+  addCommentInCard,
+  removeCommentFromCard,
+};
