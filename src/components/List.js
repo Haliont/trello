edee82482fb36ab4e3a622e51588bee88cc87ecd
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-10-30 15:57:50 +0600

    Replaced function expression on function declaration

--- src/components/List.js
+++ src/components/List.js
@@ -4,31 +4,35 @@ import Card from './Card';
 import AddCardForm from './AddCardForm';
 import TextEditor from './TextEditor';
 
-const List = ({
-  title,
-  onSetTitle,
-  onAddNewCard,
-  cards,
-  onRemoveCard,
-  onOpenCard,
-}) => (
-  <div className="List panel">
-    <TextEditor onTextSave={onSetTitle} value={title} />
-    <div className="List-Cards panel-block">
-      {cards.map(({ id, title: cardTitle, commentIds }) => (
-        <Card
-          key={id}
-          commentsCount={commentIds.length}
-          onOpen={onOpenCard(id)}
-          onRemove={onRemoveCard(id)}
-          title={cardTitle}
-        />
-      ))}
+function List(props) {
+  const {
+    title,
+    cards,
+    onSetTitle,
+    onOpenCard,
+    onAddNewCard,
+    onRemoveCard,
+  } = props;
+
+  return (
+    <div className="List panel">
+      <TextEditor onTextSave={onSetTitle} value={title} />
+      <div className="List-Cards panel-block">
+        {cards.map(({ id, title: cardTitle, commentIds }) => (
+          <Card
+            key={id}
+            title={cardTitle}
+            onOpen={onOpenCard(id)}
+            onRemove={onRemoveCard(id)}
+            commentsCount={commentIds.length}
+          />
+        ))}
+      </div>
+      <div className="List-Footer panel-block">
+        <AddCardForm onAddNewCard={onAddNewCard} />
+      </div>
     </div>
-    <div className="List-Footer panel-block">
-      <AddCardForm onAddNewCard={onAddNewCard} />
-    </div>
-  </div>
-);
+  );
+}
 
 export default List;
