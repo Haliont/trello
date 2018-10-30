dfd531847a15df5e69c24cc1818e04e2af25fdf8
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-10-30 18:31:04 +0600

    Relpaced Signup component on SignupForm

---
+++ src/components/SignupForm.js
@@ -0,0 +1,35 @@
+import './Signup.css';
+import React from 'react';
+
+function SignupForm({ onSubmit, onChange }) {
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="field">
+        <label className="label is-large" htmlFor="username">
+          Username
+        </label>
+        <div className="control has-icons-left has-icons-right">
+          <input
+            id="username"
+            type="text"
+            className="input is-large"
+            placeholder="Example: Petya"
+            onChange={onChange}
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-user" />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <p className="control">
+          <button type="submit" className="button is-success">
+            Login
+          </button>
+        </p>
+      </div>
+    </form>
+  );
+}
+
+export default SignupForm;
