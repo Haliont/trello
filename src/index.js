35fa26bc5c64e27066fa76d6a5d3f387e8b91ad2
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-11-11 16:05:57 +0600

    Added Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import 'bulma/css/bulma.min.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+// import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Board from './containers/Board';
+
+import store from './store';
+
+store.subscribe(() => {
+  const {
+    // cards,
+    lists,
+    username,
+  } = store.getState();
+
+  // localStorage.cards = JSON.stringify(cards);
+  localStorage.lists = JSON.stringify(lists);
+  localStorage.username = username;
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Board />
+  </Provider>,
+  document.getElementById('root'),
+);
