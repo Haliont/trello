c92428a054f25330e2199d4a2f2eeddf618a3a75
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2018-11-13 16:55:25 +0600

    Added propTypes for App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Modal from './Modal';
 import Board from '../containers/Board';
 import SignupForm from './SignupForm';
 
 class App extends Component {
+  static propTypes = {
+    signup: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    isRegistered: PropTypes.bool.isRequired,
+    updateUsername: PropTypes.func.isRequired,
+  };
+
   handleSignup = (event) => {
     event.preventDefault();
     const { signup, username } = this.props;
